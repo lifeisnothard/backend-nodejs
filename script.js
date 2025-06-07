1951ae421e9bafa43d0c3a5ba753111d9794d100
Luong Trong Vinh <vinhluongprogramming@gmail.com>
2025-06-07 20:45:12 +0700

    Learn JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var button = document.querySelector('button')
-
-function greeting (message) {
-	console.log(message)
-}
-
 button.onclick = function () {
-	setTimeout(() => greeting('Hello JavaScript'), 5000)
+	var a = 10
+	var b = "Noi dung ..."
+	var c
+	var d = null
+	var e = true
+	console.log(typeof a)
+	console.log(typeof b)
+	console.log(typeof c)
+	console.log(typeof d)
+	console.log(typeof e)
 }

4910dafe6d654f8cd30fc871af83e61dee7631f5
Luong Trong Vinh <vinhluongprogramming@gmail.com>
2025-06-11 18:17:06 +0700

    Update all files

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-var button = document.querySelector('button')
-button.onclick = function () {
-	var a = 10
-	var b = "Noi dung ..."
-	var c
-	var d = null
-	var e = true
-	console.log(typeof a)
-	console.log(typeof b)
-	console.log(typeof c)
-	console.log(typeof d)
-	console.log(typeof e)
+function setCookie (name, value, days = 7, path = '/') {
+	const encodedValue = encodeURIComponent(value);
+	const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
+	document.cookie = `${name}=${encodedValue}; expires=${expires}; path=${path}`
+}
+
+setCookie('name','Vinh', 7)
+
+function getCookiesAsObject() {
+	const cookies = document.cookie.split('; ');
+	const cookieObj = {};
+
+	cookies.forEach(cookie => {
+		const [name, value] = cookie.split('=');
+		cookieObj[name] = decodeURIComponent(value);
+	});
+
+	return cookieObj;
+}
+
+console.log(getCookiesAsObject());
+
+function deleteCookie(name, path = '/') {
+	document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=${path}`;
 }
